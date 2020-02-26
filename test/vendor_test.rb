b6e423e042dbb4385d31cf51f2d6513e52a0bd12
Alex Latham <alexlthm@gmail.com>
2020-02-26 10:11:22 -0700

    Add first tests for Vendor

---
+++ test/vendor_test.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require './lib/vendor'
+
+class VendorTest < Minitest::Test
+  def setup
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  def test_it_can_exist
+    assert_instance_of Vendor, @vendor
+  end
+
+  def test_it_has_attributes
+    assert_equal "Rocky Mountain Fresh", @vendor.name
+    assert_equal ({}), @vendor.inventory
+  end
+end
