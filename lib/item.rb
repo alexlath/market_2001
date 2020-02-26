f8a38bf5a6d62e731e376aa67c261336e92aa855
Alex Latham <alexlthm@gmail.com>
2020-02-26 10:49:15 -0700

    Modify price to exclude non-digit characters

--- lib/item.rb
+++ lib/item.rb
@@ -3,6 +3,6 @@ class Item
 
   def initialize(item_params)
     @name = item_params[:name]
-    @price = item_params[:price][-4..-1].to_f
+    @price = item_params[:price].gsub(/[^\d\.]/,'').to_f
   end
 end
